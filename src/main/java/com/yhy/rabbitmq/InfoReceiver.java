9c3d0de875ff98a83f1ad9bced9f26ec46fc068a
atlantis0617 <973549268@qq.com>
2018-08-27 10:06:51 +0800

    rabbitmq初始化

---
+++ src/main/java/com/yhy/rabbitmq/InfoReceiver.java
@@ -0,0 +1,26 @@
+package com.yhy.rabbitmq;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(bindings=@QueueBinding(
+				value=@Queue(value="${mq.config.queue.info}",autoDelete="true"),
+				exchange=@Exchange(value="${mq.config.exchange}",type=ExchangeTypes.TOPIC),
+				key="*.log.info"
+			   )
+			   )
+public class InfoReceiver {
+	
+	
+	@RabbitHandler
+	public void process(String msg) {
+		System.out.println("接收到info日志r----"+msg);
+	}
+
+}
